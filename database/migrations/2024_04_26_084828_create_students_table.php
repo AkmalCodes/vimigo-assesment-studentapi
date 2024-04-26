5ee875460387a0494c09e0404deabfe47056de7c
Akmal Hisyam <akmalhisyam97@gmail.com>
2024-04-26 16:52:50 +0800

    added students table

--- database/migrations/2024_04_26_084828_create_students_table.php
+++ database/migrations/2024_04_26_084828_create_students_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('address')->nullable();
+            $table->string('study_course');
             $table->timestamps();
         });
     }
